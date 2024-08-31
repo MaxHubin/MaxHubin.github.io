1cc239c63cac324c96882c2f9ff847bebfbf030c
max <max.hubin@gmail.com>
2024-08-31 17:26:25 +0200

    1

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+    // Можно добавить логику кэширования
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
+self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetching:', event.request.url);
+    event.respondWith(fetch(event.request));
 });
